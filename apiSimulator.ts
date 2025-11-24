cf9c2e8ba8235ccb9b8c2a8438b80d76456d0b70
JuanSalda91 <juansalda9131@gmail.com>
2025-11-24 01:08:17 -0500

    apiSim updated with custom errors

--- apiSimulator.ts
+++ apiSimulator.ts
@@ -1,5 +1,8 @@
 // ----- apiSimulator ----- //
 
+// --- Import errors --- //
+import { NetworkError, DataError } from "./errors.ts";
+
 /**
  * 1. Fetching Product Catalog
  * --- Simulates getting a list of product from database.
@@ -14,15 +17,19 @@ export const fecthProductCatalog = (): Promise<{ id: number; name: string; price
         setTimeout(() => {
             // --- Math.ramdon() gives a number between 0 to 1
             // --- If it is less than 0.9 (90% chance), it succeed.
-            if (Math.random() < 0.9) {
+            const randomValue = Math.random();
+            if (randomValue < 0.8) {
                 resolve([
                     { id: 1, name: "Laptop", price: 1200 },
                     { id: 2, name: "Headphones", price: 200 },
                     { id: 3, name: "Keyboard", price: 100 },
                 ]);
+            } else if (randomValue < 0.9) {
+                // --- 10% chance of network error
+                reject(new NetworkError("Failed to connect to Product Catalog Server."));
             } else {
-                // --- 10% chance to fail
-                reject("Failes to fetch product catalog");
+                // --- 10% chance od Data Error
+                reject(new DataError("Product Catalog data is corrupt."))
             }
         }, 1000); // --- 1000 milliseconds = 1 second delay
     });
@@ -41,13 +48,14 @@ export const fetchProductReviews = (productId: number): Promise<{ productId: num
         console.log(`Simulating: Fetching reviews for product ID ${productId}...`);
 
         setTimeout(() => {
-            if (Math.random() < 0.9) {
+            const randomValue = Math.random();
+            if (randomValue < 0.9) {
                 resolve([
                     { productId: productId, review: "Great product!" },
                     { productId: productId, review: "Good value for the money." },
                 ]);
             } else {
-                reject(`Failed to fetch reviews for product ID ${productId}`);
+                reject(new NetworkError(`Failed to fetch reviews for product ID ${productId}`));
             }
         }, 1500); // --- 1.5 seconds delay
     });
@@ -72,7 +80,7 @@ export const fetchSalesReport = (): Promise<{ totalSales: number; unitSold: numb
                     averagePrice: 333,
                 });
             } else {
-                reject("Failed to fetch sales report");
+                reject(new DataError("Failed to fetch sales report"));
             }
         }, 1000); // --- 1 second delay
     });
